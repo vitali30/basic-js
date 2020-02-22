70a120a6368a3b3f7cad022255fbaf31d07553aa
vitali30 <60606326+vitali30@users.noreply.github.com>
2020-02-23 00:48:42 +0300

    add: count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -4,18 +4,8 @@ module.exports = function countCats(matrix) {
   for (let index of matrix){
     result.push(index);
   }
-  result = result.join().split(","); 
-for (let index of result){
+for (let index of result.join().split(",")){
   if (index == "^^")num++;
 }
-
-
-
  return (num)
-};
-
-let matrix = [
-  [1, 2, "^^", 4, 5],
-  [6, "^^", 8, 9],
-  ];
-console.log(module.exports (matrix));
+}
