5b04ea611fdf92875a0b0cd72330cdf2466f5f47
vitali30 <60606326+vitali30@users.noreply.github.com>
2020-02-23 00:47:13 +0300

    add: count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,21 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let result = [];
+  let num = 0;
+  for (let index of matrix){
+    result.push(index);
+  }
+  result = result.join().split(","); 
+for (let index of result){
+  if (index == "^^")num++;
+}
+
+
+
+ return (num)
 };
+
+let matrix = [
+  [1, 2, "^^", 4, 5],
+  [6, "^^", 8, 9],
+  ];
+console.log(module.exports (matrix));
