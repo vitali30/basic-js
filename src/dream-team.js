c28e2e9ba2bb0b5decdf6cf7d77674471cfa495c
vitali30 <60606326+vitali30@users.noreply.github.com>
2020-02-24 00:44:23 +0300

    add: file dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,14 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+      let arRes = [];
+        if (!Array.isArray(members))return (false) 
+    for (let array of members){
+        if (typeof array == "string") {
+            for (let value of array){
+                if (value !== ' '){
+                    arRes.push(value);break
+                }
+            }
+        }
+    }
+    return (arRes.join('').toUpperCase().split('').sort().join(''));
 };
