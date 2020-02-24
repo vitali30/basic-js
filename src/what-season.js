a0ff32a46cab2ee635642039cf20373391398c0c
vitali30 <60606326+vitali30@users.noreply.github.com>
2020-02-24 22:12:21 +0300

    add: file what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,23 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (!date) return ('Unable to determine the time of year!');
+    else if (Object.prototype.toString.call(date)=='[object Date]'){
+        switch (date.getMonth()){
+            case 11:
+            case 0:
+            case 1:
+                return ('winter');
+            case 2:
+            case 3:
+            case 4:
+                return ('spring');
+            case 5:
+            case 6:
+            case 7:
+                return ('summer');
+            default:
+                return ('autumn')
+        }
+    }
+    else throw Error
 };
+
