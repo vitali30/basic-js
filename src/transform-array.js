ad87357bc61e8140e216129847f0cfc516b162ae
vitali30 <60606326+vitali30@users.noreply.github.com>
2020-02-24 23:34:19 +0300

    add:file transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,12 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) throw error;
+    let arrNew = [];
+    for (let i = 0 ; i < arr.length ; i ++){
+        if (arr[i] == "--discard-next"){if (i+1 < arr.length) i++}
+        else if (arr[i] == "--discard-prev"){if (i>0) arrNew.pop()}
+        else if (arr[i] == "--double-next"){if (i+1 < arr.length) arrNew.push(arr[i+1])}
+        else if (arr[i] == "--double-prev"){if (i>0) arrNew.push(arr[i-1])}
+        else arrNew.push(arr[i]);
+    }
+return (arrNew);
 };
